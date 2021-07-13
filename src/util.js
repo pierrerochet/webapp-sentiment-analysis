7a2844ea1938edc13cac1b99f566873660e3c52a
Pierre Rochet <pierre-rochet@outlook.fr>
2021-07-14 01:46:50 +0200

    update aws workflow

--- src/util.js
+++ src/util.js
@@ -2,7 +2,8 @@ import fetch from "node-fetch";
 
 export async function getSentiment(value) {
   // const url = "http://localhost:8000/api/v1/sentiment/predict";
-  const url = "http://localhost:8080/api/v1/sentiment/predict";
+  // const url = "http://localhost:8080/api/v1/sentiment/predict";
+  const url = 'http://ec2-15-236-60-229.eu-west-3.compute.amazonaws.com/api/v1/sentiment/predict'
   const res = await fetch(url, {
     method: "POST",
     body: JSON.stringify({ text: value }),
