b135656fe6f14d5297759e07baddfced580f2fad
Pierre Rochet <pierre-rochet@outlook.fr>
2021-07-14 10:47:56 +0200

    add gh-pages config

--- src/util.js
+++ src/util.js
@@ -1,9 +1,7 @@
 import fetch from "node-fetch";
 
 export async function getSentiment(value) {
-  // const url = "http://localhost:8000/api/v1/sentiment/predict";
-  // const url = "http://localhost:8080/api/v1/sentiment/predict";
-  const url = 'http://ec2-15-236-60-229.eu-west-3.compute.amazonaws.com/api/v1/sentiment/predict'
+  const url = process.env.REACT_APP_API_ENDPOINT
   const res = await fetch(url, {
     method: "POST",
     body: JSON.stringify({ text: value }),
